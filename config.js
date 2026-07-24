0dce89458734bbf490e6e34e9dbe7b4f71c89a6e
JON RIO MUÑOZ <jonrio11@gmail.com>
2026-07-24 22:29:08 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Token de cliente de Mapillary (empieza por "MLY|").
 // Es el mismo token que ya usas en guesstadium: puedes copiar y pegar
 // el valor tal cual desde tu config.js de aquel proyecto.
-const MAPILLARY_TOKEN = "MLY|26846645948342864|8fcbe73a76c521f4048c1b566650a3ba";
+const MAPILLARY_TOKEN = "MLY|26846645948342864|9e2d92414c0d8bae09bdcb134e7e7945";
